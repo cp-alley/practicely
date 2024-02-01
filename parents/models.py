15b42410e9bd1b888ef63708ccf6f82757ad92a2
Christopher Alley <calleyviola@gmail.com>
2024-01-31 20:50:41 -0500

    Scaffold teachers, students, parents models

--- parents/models.py
+++ parents/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from users.models import User
 
 # Create your models here.
-class Teacher(User):
+class Parent(User):
     pass
--- practicely/settings.py
+++ practicely/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 
     'users',
     'teachers',
+    'students',
+    'parents',
 ]
 
 MIDDLEWARE = [
